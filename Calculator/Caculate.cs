1548957d95255cb87333bbf832b87a8c4d79c7e0
chandrashekhar54 <chandrashekar.mh@graniteriverlabs.in>
2023-11-17 12:57:55 +0530

    updated

--- Calculator/Caculate.cs
+++ Calculator/Caculate.cs
@@ -60,7 +60,7 @@ namespace Calculator
             return a * b;
         }
 
-        // updated
+        // updated code
         public static double Divide(double a, double b)
         {
             if (b != 0)
